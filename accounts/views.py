d4a578905544057959174ca7c96c9cbbf1b88500
aminnjz <njzamin@gmail.com>
2024-01-14 12:20:52 +0330

    after cours 19 - logout !

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
+from typing import ParamSpecArgs
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login,logout
 
 
 
@@ -14,6 +15,7 @@ def signup_view (request):
     else :
             form = UserCreationForm()
     return render(request, 'accounts/signup.html',  {'form': form})
+
 
 
 
@@ -32,3 +34,11 @@ def login_view (request):
         form = AuthenticationForm()
     return render(request, 'accounts/login.html',{'form':form})        
     
+    
+    
+    
+def logout_view (request):
+    if request.method == 'POST':
+        logout(request)
+        
+        return redirect('articles:list')   
